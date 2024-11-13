f9f160a810c7569d0f540204e43995492c65e20d
Sushant <sushantcod3s@gmail.com>
2024-11-13 16:26:32 +0530

    Add the test for invalid plan in signup api handler

--- handler/signup.go
+++ handler/signup.go
@@ -10,6 +10,26 @@ import (
 )
 
 var ErrInvalidEmail = errors.New("invalid email")
+var ErrInvalidPlan = errors.New("invalid plan, supported plans are basic, advanced, or enterprise")
+
+type Plan string
+
+const BasicPlan = "basic"
+const AdvancePlan = "advance"
+const EnterprisePlan = "enterprise"
+
+func getPlanFromString(plan string) (Plan, error) {
+	switch plan {
+	case BasicPlan:
+		return BasicPlan, nil
+	case AdvancePlan:
+		return AdvancePlan, nil
+	case EnterprisePlan:
+		return EnterprisePlan, nil
+	default:
+		return "", ErrInvalidPlan
+	}
+}
 
 func SignupHandler(c *gin.Context) {
 	var signupPaylod types.SignupPayload
@@ -19,14 +39,25 @@ func SignupHandler(c *gin.Context) {
 		return
 	}
 
-	if !isEmailValid(signupPaylod.Email) {
-		c.JSON(400, gin.H{"errorMessage": ErrInvalidEmail.Error()})
+	_, err = isEmailValid(signupPaylod.Email)
+	if err != nil {
+		c.JSON(400, gin.H{"errorMessage": err.Error()})
 		return
 	}
+
+	_, err = getPlanFromString(signupPaylod.Plan)
+	if err != nil {
+		c.JSON(400, gin.H{"errorMessage": err.Error()})
+		return
+	}
+
 }
 
-func isEmailValid(email string) bool {
+func isEmailValid(email string) (bool, error) {
 	regex := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
 	re := regexp.MustCompile(regex)
-	return re.MatchString(email)
+	if re.MatchString(email) {
+		return true, nil
+	}
+	return false, ErrInvalidEmail
 }
--- handler/signup_test.go
+++ handler/signup_test.go
@@ -39,7 +39,51 @@ func TestSignupHandlerForInvalidEmail(t *testing.T) {
 	// calling the signup handler
 	SignupHandler(c)
 
-	// asserting the valies
+	// asserting the values
+	if expStatus != w.Code {
+		t.Errorf("Expected %d but got %d", expStatus, w.Code)
+	}
+
+	var respBody map[string]string
+	err = json.Unmarshal(w.Body.Bytes(), &respBody)
+	if err != nil {
+		t.Fatalf("Error while unmarshalling response: %v", err)
+	}
+
+	if expErrorMessage != respBody["errorMessage"] {
+		t.Errorf("Expected %v but got %v", expStatus, w.Code)
+	}
+
+}
+func TestSignupHandlerForInvalidPlan(t *testing.T) {
+	// unhappy path of invalid email
+	payload := types.SignupPayload{
+		Name:  "abc corp",
+		Email: "test@abc.corp",
+		Plan:  "invalid-plan",
+	}
+	expStatus := http.StatusBadRequest
+	// expResp := `{
+	// 	"errorMessage”: "invalid email"
+	// }`
+	expErrorMessage := "invalid plan, supported plans are basic, advanced, or enterprise"
+
+	// marhalling the payload into json
+	body, err := json.Marshal(payload)
+	if err != nil {
+		t.Fatalf("Error while marshalling payload: %v", err)
+	}
+
+	// preparing the test
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+	c.Request = httptest.NewRequest("POST", "/signup", bytes.NewBuffer([]byte(body)))
+	c.Request.Header.Set("Content-Type", "application/json")
+
+	// calling the signup handler
+	SignupHandler(c)
+
+	// asserting the values
 	if expStatus != w.Code {
 		t.Errorf("Expected %d but got %d", expStatus, w.Code)
 	}
