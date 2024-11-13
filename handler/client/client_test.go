35c848ffe9e3e73943f1849e82fed52228df858e
Sushant <sushantcod3s@gmail.com>
2024-11-13 17:21:12 +0530

    Refactor the tests of signup api handler into table driven test

--- handler/client/client_test.go
+++ handler/client/client_test.go
@@ -9,90 +9,58 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/justsushant/one2n-go-bootcamp/go-ekyc/types"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestSignupHandlerForInvalidEmail(t *testing.T) {
-	// unhappy path of invalid email
-	payload := types.SignupPayload{
-		Name:  "abc corp",
-		Email: "test@abc@corp",
-		Plan:  "basic",
-	}
-	expStatus := http.StatusBadRequest
-	// expResp := `{
-	// 	"errorMessage”: "invalid email"
-	// }`
-	expErrorMessage := "invalid email"
-
-	// marhalling the payload into json
-	body, err := json.Marshal(payload)
-	if err != nil {
-		t.Fatalf("Error while marshalling payload: %v", err)
-	}
-
-	// preparing the test
-	w := httptest.NewRecorder()
-	c, _ := gin.CreateTestContext(w)
-	c.Request = httptest.NewRequest("POST", "/signup", bytes.NewBuffer([]byte(body)))
-	c.Request.Header.Set("Content-Type", "application/json")
-
-	// calling the signup handler
-	SignupHandler(c)
-
-	// asserting the values
-	if expStatus != w.Code {
-		t.Errorf("Expected %d but got %d", expStatus, w.Code)
+func TestSignupHandler(t *testing.T) {
+	tt := []struct {
+		name          string
+		payload       types.SignupPayload
+		expStatusCode int
+		expResponse   string
+	}{
+		{
+			name: "invalid email case",
+			payload: types.SignupPayload{
+				Name:  "abc corp",
+				Email: "test@abc@corp",
+				Plan:  "basic",
+			},
+			expStatusCode: http.StatusBadRequest,
+			expResponse:   `{"errorMessage":"invalid email"}`,
+		},
+		{
+			name: "invalid plan case",
+			payload: types.SignupPayload{
+				Name:  "abc corp",
+				Email: "test@abc.corp",
+				Plan:  "invalid-plan",
+			},
+			expStatusCode: http.StatusBadRequest,
+			expResponse:   `{"errorMessage":"invalid plan, supported plans are basic, advanced, or enterprise"}`,
+		},
 	}
 
-	var respBody map[string]string
-	err = json.Unmarshal(w.Body.Bytes(), &respBody)
-	if err != nil {
-		t.Fatalf("Error while unmarshalling response: %v", err)
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			// marhalling the payload into json
+			body, err := json.Marshal(tc.payload)
+			if err != nil {
+				t.Fatalf("Error while marshalling payload: %v", err)
+			}
+
+			// preparing the test
+			w := httptest.NewRecorder()
+			c, _ := gin.CreateTestContext(w)
+			c.Request = httptest.NewRequest("POST", "/signup", bytes.NewBuffer([]byte(body)))
+			c.Request.Header.Set("Content-Type", "application/json")
+
+			// calling the signup handler
+			SignupHandler(c)
+
+			// asserting the values
+			assert.Equal(t, tc.expStatusCode, w.Code)
+			assert.JSONEq(t, tc.expResponse, w.Body.String())
+		})
 	}
-
-	if expErrorMessage != respBody["errorMessage"] {
-		t.Errorf("Expected %v but got %v", expStatus, w.Code)
-	}
-
-}
-func TestSignupHandlerForInvalidPlan(t *testing.T) {
-	// unhappy path of invalid email
-	payload := types.SignupPayload{
-		Name:  "abc corp",
-		Email: "test@abc.corp",
-		Plan:  "invalid-plan",
-	}
-	expStatus := http.StatusBadRequest
-	expErrorMessage := "invalid plan, supported plans are basic, advanced, or enterprise"
-
-	// marhalling the payload into json
-	body, err := json.Marshal(payload)
-	if err != nil {
-		t.Fatalf("Error while marshalling payload: %v", err)
-	}
-
-	// preparing the test
-	w := httptest.NewRecorder()
-	c, _ := gin.CreateTestContext(w)
-	c.Request = httptest.NewRequest("POST", "/signup", bytes.NewBuffer([]byte(body)))
-	c.Request.Header.Set("Content-Type", "application/json")
-
-	// calling the signup handler
-	SignupHandler(c)
-
-	// asserting the values
-	if expStatus != w.Code {
-		t.Errorf("Expected %d but got %d", expStatus, w.Code)
-	}
-
-	var respBody map[string]string
-	err = json.Unmarshal(w.Body.Bytes(), &respBody)
-	if err != nil {
-		t.Fatalf("Error while unmarshalling response: %v", err)
-	}
-
-	if expErrorMessage != respBody["errorMessage"] {
-		t.Errorf("Expected %v but got %v", expStatus, w.Code)
-	}
-
 }
