41d9208f44ef1834e6801c989eb65bf32fb77caf
Sushant <sushantcod3s@gmail.com>
2024-11-24 21:38:53 +0530

    Add the PrepareCSV method and tests in CronJob module

--- cronjob/cronjob_service.go
+++ cronjob/cronjob_service.go
@@ -1,11 +1,20 @@
 package cronjob
 
-import "github.com/justsushant/one2n-go-bootcamp/go-ekyc/types"
+import (
+	"bytes"
+	"errors"
+	"log"
+
+	"github.com/gocarina/gocsv"
+	"github.com/justsushant/one2n-go-bootcamp/go-ekyc/types"
+)
 
 // TODO: change the name of interface
 type CronJobServiceInterface interface {
 	PrepareCSV([]*types.ClientReport) ([]byte, error)
 }
+
+var ErrMissingReports = errors.New("no reports found")
 
 type CronJobService struct{}
 
@@ -13,6 +22,20 @@ func NewCronJobService() *CronJobService {
 	return &CronJobService{}
 }
 
-func (s *CronJobService) PrepareCSV(*[]types.ClientReport) ([]byte, error) {
-	return nil, nil
+func (s *CronJobService) PrepareCSV(data []*types.ClientReport) ([]byte, error) {
+	// length check
+	if len(data) == 0 {
+		return nil, ErrMissingReports
+	}
+
+	// buffer to hold the csv
+	var buf bytes.Buffer
+
+	// writing the data to buffer
+	if err := gocsv.Marshal(&data, &buf); err != nil {
+		log.Printf("Error while writing CSV to buffer: %s\n", err.Error())
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
 }
---
+++ cronjob/cronjob_service_test.go
@@ -0,0 +1,98 @@
+package cronjob
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/justsushant/one2n-go-bootcamp/go-ekyc/types"
+)
+
+func TestPrepareCSV(t *testing.T) {
+	tt := []struct {
+		name    string
+		data    []*types.ClientReport
+		expResp string
+		expErr  error
+	}{
+		{
+			name:    "zero report objects",
+			data:    []*types.ClientReport{},
+			expResp: "",
+			expErr:  ErrMissingReports,
+		},
+		{
+			name: "one report object",
+			data: []*types.ClientReport{
+				{
+					ClientID:          "1",
+					Name:              "client1",
+					Plan:              "basic",
+					Date:              "2024-11-22",
+					TotalFaceMatch:    "5",
+					TotalOcr:          "2",
+					TotalImgStorageMB: "10",
+					TotalAPIUsageCost: "12",
+					TotalStorageCost:  "10",
+				},
+			},
+			expResp: "client_id,name,plan,date,total_face_match_for_day,total_ocr_for_da,total_image_storage_in_mb,api_usage_cost_usd,storage_cost_usd\n1,client1,basic,2024-11-22,5,2,10,12,10\n",
+			expErr:  nil,
+		},
+		{
+			name: "two report objects",
+			data: []*types.ClientReport{
+				{
+					ClientID:          "1",
+					Name:              "client1",
+					Plan:              "basic",
+					Date:              "2024-11-22",
+					TotalFaceMatch:    "5",
+					TotalOcr:          "2",
+					TotalImgStorageMB: "10",
+					TotalAPIUsageCost: "12",
+					TotalStorageCost:  "10",
+				},
+				{
+					ClientID:          "2",
+					Name:              "client2",
+					Plan:              "basic",
+					Date:              "2024-11-22",
+					TotalFaceMatch:    "7",
+					TotalOcr:          "4",
+					TotalImgStorageMB: "12",
+					TotalAPIUsageCost: "14",
+					TotalStorageCost:  "12",
+				},
+			},
+			expResp: "client_id,name,plan,date,total_face_match_for_day,total_ocr_for_da,total_image_storage_in_mb,api_usage_cost_usd,storage_cost_usd\n1,client1,basic,2024-11-22,5,2,10,12,10\n2,client2,basic,2024-11-22,7,4,12,14,12\n",
+			expErr:  nil,
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			service := NewCronJobService()
+			csvBytes, err := service.PrepareCSV(tc.data)
+
+			if tc.expErr != nil {
+				if err == nil {
+					t.Errorf("Expected error but didn't got one\n")
+				}
+
+				if !errors.Is(err, tc.expErr) {
+					t.Errorf("Expected error %s but got %s\n", tc.expErr, err)
+				}
+
+				return
+			}
+
+			if err != nil {
+				t.Fatalf("Unexpected error: %s", err.Error())
+			}
+
+			if string(csvBytes) != tc.expResp {
+				t.Errorf("Expected %q but got %q", tc.expResp, string(csvBytes))
+			}
+		})
+	}
+}
